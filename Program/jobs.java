b439187bfbae8d12e2985c7930e2609baa21f484
Dharaneish V C <82216772+dharaneishvc@users.noreply.github.com>
2022-01-04 19:19:45 +0530

    created jobs.java
    classes include company, jobs, placements

---
+++ Program/jobs.java
@@ -0,0 +1,20 @@
+class Company
+{
+	public int Company_ID;
+	public String Company_name;
+	public String Website;
+}
+class Jobs
+{
+	public int Job_ID;
+	public String role;
+	public String[] Eligible_Branches;
+	public String[] Qualifications;
+	public int no_of_hirings;
+}
+class Placements
+{
+	public String Roll_No;
+	public int Job_ID;
+	public Date Date_placed; //read date and time object
+}
